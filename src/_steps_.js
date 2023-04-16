2954dd5840ff1143587a0bd7dd5f11acfa2e8152
md-abir-alahe <mdabiralahe@gmail.com>
2023-04-16 20:34:29 +0600

    email password authentication project setup

---
+++ src/_steps_.js
@@ -0,0 +1,9 @@
+/* *******
+1. authentication
+2. authorization
+3. sign in === log in
+4. sign up === register
+5. 
+
+
+*/
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import Main from './components/Layout/Main';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Register from './components/Login/Register/Register';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>,
+      },
+      {
+        path: '/login',
+        element: <Login></Login>,
+      },
+      {
+        path: '/register',
+        element: <Register></Register>
+      },
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
