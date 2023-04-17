93c593338be7248699a724b98e8de4f66c94e2d3
md-abir-alahe <mdabiralahe@gmail.com>
2023-04-17 19:31:48 +0600

    simple form, input, change, blur, submit, preventDefault

--- src/components/Login/Register/Register.jsx
+++ src/components/Login/Register/Register.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Register = () => {
+    const [email, setEmail] = useState('');
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        // console.log(event);
+        // console.log(event.target);
+        // console.log(event.target.email);
+        // console.log(event.target.email.value);
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        console.log(email, password);
+        }
+
+    // get value with onChang event handler
+    const handleEmailChange = (event) => {
+        // console.log(event.target.value);
+        // setEmail(event.target.value)
+    }
+    // get value with onBlur event handler
+    const handlePasswordBlur = (event) => {
+        // console.log(event.target.value);
+    }
+
+
     return (
         <div>
             <h3>Please Register</h3>
-            <form>
-                <input type="email" name="email" id="email" placeholder='Your Email' />
+            <form onSubmit={handleSubmit}>
+                <input onChange={handleEmailChange} type="email" name="email" id="email" placeholder='Your Email' />
                 <br />
-                <input type="password" name="password" id="password" placeholder='Your Password' />
+                <input  onBlur={handlePasswordBlur} type="password" name="password" id="password" placeholder='Your Password' />
                 <br />
                 <input type="submit" value="Register" />
             </form>
